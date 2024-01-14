a1ee522ee8d84bbaaf139159a348f26654b6394d
Martin Tillmann <mtillmann@gmail.com>
2024-01-14 09:31:16 +0100

    Refactor build script and update packaging process

--- make-package.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf dist
-mkdir dist
-version=$(cat manifest.json | grep -oP 'version": "\K.*(?=")')
-name="dist/package-$version.zip"
-zip -r $name * -x dist make-package.sh
--- tools/build.sh
+++ tools/build.sh
@@ -4,11 +4,13 @@ mkdir dist
 version=$(cat manifest.json | grep -oP 'version": "\K.*(?=")')
 #name="dist/package-$version.zip"
 #zip -r $name * -x dist make-package.sh
-rsync --exclude=dist  --exclude=tools --exclude=build.sh --exclude=make-package.sh --exclude=.git --exclude=.gitignore --exclude=readme.md --exclude=LICENSE -r . dist/color-helper
-# chrome
+rsync --exclude=dist --exclude=tools --exclude=.git --exclude=.gitignore --exclude=readme.md --exclude=LICENSE -r . dist/color-helper
+# chrome crx
 $(which google-chrome) --pack-extension=dist/color-helper
 mv dist/color-helper.crx dist/color-helper-$version.crx
 mv dist/color-helper.pem dist/color-helper-$version.pem
+# chrome zip
+cd dist/color-helper && zip -rq ../color-helper-$version.zip * && cd ../..
 # firefox
 node ./tools/firefoxify.mjs
 cd dist/color-helper && zip -rq ../color-helper-$version.xpi * && cd ../..
