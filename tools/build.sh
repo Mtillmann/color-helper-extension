7c3ea54eedb7ac7d0e9ca8bcef2180160cc6cce6
Martin Tillmann <mtillmann@gmail.com>
2024-01-14 20:06:27 +0100

    Update build script to include instructions for packaging Chrome and Firefox extensions

--- tools/build.sh
+++ tools/build.sh
@@ -5,14 +5,19 @@ version=$(cat manifest.json | grep -oP 'version": "\K.*(?=")')
 #name="dist/package-$version.zip"
 #zip -r $name * -x dist make-package.sh
 rsync --exclude=dist --exclude=tools --exclude=.git --exclude=.gitignore --exclude=readme.md --exclude=LICENSE -r . dist/color-helper
-# chrome crx
-$(which google-chrome) --pack-extension=dist/color-helper
-mv dist/color-helper.crx dist/color-helper-$version.crx
-mv dist/color-helper.pem dist/color-helper-$version.pem
+# chrome crx - not used because zip is less hassle
+# $(which google-chrome) --pack-extension=dist/color-helper
+# mv dist/color-helper.crx dist/color-helper-$version.crx
+# mv dist/color-helper.pem dist/color-helper-$version.pem
 # chrome zip
 cd dist/color-helper && zip -rq ../color-helper-$version.zip * && cd ../..
 # firefox
 node ./tools/firefoxify.mjs
 cd dist/color-helper && zip -rq ../color-helper-$version.xpi * && cd ../..
+echo "Chrome extension: dist/color-helper-$version.zip"
+echo "Chrome Web Store Developer Dashboard: https://chrome.google.com/webstore/devconsole/"
+echo "------"
+echo "Firefox extension: dist/color-helper-$version.xpi"
+echo "Firefox Add-on Developer Hub: https://addons.mozilla.org/en-US/developers/addons"
 #rm -rf dist/color-helper
 
