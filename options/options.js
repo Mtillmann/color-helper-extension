912d15c1368dc87356d7a94a2e4cd47e3765508d
Martin Tillmann <mtillmann@gmail.com>
2024-02-09 14:06:44 +0100

    Update button IDs in options/index.html

--- options/options.js
+++ options/options.js
@@ -1,4 +1,4 @@
-const DEFAULTSTATE = {
+let DEFAULTSTATE = {
     //this is defined in background/index.js
 };
 
@@ -59,7 +59,7 @@ function apply() {
     document.querySelector(`[name="colorTheme"][value="${STATE.colorTheme}"]`).setAttribute('checked', true);
 
     document.querySelector(`[name="analyzerBackgroundMode"][value="${STATE.analyzerBackgroundMode}"]`).setAttribute('checked', true);
-    
+
 
 
     document.querySelectorAll('[data-bind-property]').forEach(el => {
@@ -83,25 +83,25 @@ function apply() {
     document.querySelectorAll('[data-show-for-property]').forEach(el => {
         const show = STATE[el.dataset.showForProperty];
         const className = el.dataset.displayClass ?? 'd-block';
-        if(show){
+        if (show) {
             el.classList.add(className);
             el.classList.remove('d-none')
-        }else{
+        } else {
             el.classList.add('d-none');
             el.classList.remove(className)
         }
-        
+
     });
 
     store();
 }
 
-async function store(){
+async function store() {
     const state = Object.keys(DEFAULTSTATE).reduce((all, key) => {
         all[key] = STATE[key];
         return all;
     }, {});
-    await chrome.storage.sync.set(state);    
+    await chrome.storage.sync.set(state);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -109,14 +109,15 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     const settings = await chrome.storage.sync.get();
     for (const key in settings) {
-        DEFAULTSTATE[key] = settings[key];
         STATE[key] = settings[key];
     }
+
+    DEFAULTSTATE = settings.defaultState;
 
 
     ['change', 'input'].forEach(eventName => {
         document.addEventListener(eventName, e => {
-            const targetType = e.target.getAttribute('type');            
+            const targetType = e.target.getAttribute('type');
             const property = targetType === 'radio' ? e.target.name : e.target.dataset.bindProperty;
             const hint = e.target.dataset.typeHint;
 
@@ -133,184 +134,28 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     chrome.commands.getAll((commands) => {
         STATE.shortcut = commands.find((command) => command.name === 'color-helper')?.shortcut
+        apply();
     })
 
     apply();
 
-});
-
-document.addEventListener('_____alpine:init', () => {
-    Alpine.data('settings', () => {
-
-        return {
-
-            maxPixels: 100000,
-            maxPixelsFormatted: '',
-
-            highlightColorShade: true,
-
-            reduceUnmatchedOpacity: true,
-            unmatchedOpacity: 50,
-            unmatchedOpacityFormatted: '50%',
-            disableUnmatchedOpacity: false,
-
-            desaturateUnmatched: true,
-            unmatchedSaturation: 50,
-            unmatchedSaturationFormatted: '50%',
-            disableUnmatchedSaturation: false,
-
-            outlineMatched: true,
-            outlineColor: '#000000',
-            disableOutlineColor: false,
-
-            colorTheme: 'Canvas',
-
-            analyzerBackground: 'auto',
-            showCustomAnalyzerBackgroundPicker: false,
-
-            logTimings: false,
-
-            useCompatMode: false,
-
-            showShadePrefix: true,
-
-            highlightOptionsDisabled: false,
-
-            pauseOnClick: true,
-
-            shortcut: '',
-
-
-            VERSION: chrome.runtime.getManifest().version,
-
-            defaultState: {
-                
-            },
-
-            refresh() {
-                //due to CSP issues no expression can be used in the html...
-                this.maxPixelsFormatted = this.maxPixels.toLocaleString();
-                this.highlightOptionsDisabled = !this.highlightColorShade;
-                this.unmatchedOpacityFormatted = this.unmatchedOpacity + '%';
-                this.unmatchedSaturationFormatted = this.unmatchedSaturation + '%';
-                this.disableOutlineColor = !this.outlineMatched || this.highlightOptionsDisabled;
-                this.disableUnmatchedOpacity = !this.reduceUnmatchedOpacity || this.highlightOptionsDisabled;
-                this.disableUnmatchedSaturation = !this.desaturateUnmatched || this.highlightOptionsDisabled;
-
-
-
-
-                document.querySelector(`[name="colorTheme"][value="${this.colorTheme}"]`).setAttribute('checked', true);
-
-                if (this.analyzerBackground === 'auto') {
-                    this.showCustomAnalyzerBackgroundPicker = false;
-                    document.querySelector(`[name="analyzerBackground"][value="auto"]`).setAttribute('checked', true);
-                } else {
-                    this.showCustomAnalyzerBackgroundPicker = true;
-                    document.querySelector(`[name="analyzerBackground"][value="custom"]`).setAttribute('checked', true);
-                }
-
-            },
-            async store() {
-                const state = Object.keys(this.defaultState).reduce((all, key) => {
-                    all[key] = this[key];
-                    return all;
-                }, {});
-                await chrome.storage.sync.set(state);
-            },
-            async init() {
-
-                const settings = await chrome.storage.sync.get();
-                for (const key in settings) {
-                    this[key] = settings[key];
-                }
-
-                this.$nextTick(() => {
-                    this.refresh();
-                });
-
-                chrome.commands.getAll((commands) => {
-                    this.shortcut = commands.find((command) => command.name === 'color-helper')?.shortcut
-                })
-            },
-            resetSettings() {
-                Object.entries(this.defaultState).forEach(([key, value]) => {
-                    value = JSON.parse(JSON.stringify(value));
-                    this[key] = value;
-
-                });
-
-                this.refresh();
-                this.store();
-            },
-            showShortcutsPage() {
-                chrome.tabs.create({ url: 'chrome://extensions/shortcuts' });
-            },
-            updateUnmatchedSaturation(e) {
-                this.unmatchedSaturation = parseFloat(e.target.value);
-                this.refresh();
-                this.store();
-            },
-            updateUnmatchedOpacity(e) {
-                this.unmatchedOpacity = parseFloat(e.target.value);
-                this.refresh();
-                this.store();
-            },
-            updateMaxPixels(e) {
-                this.maxPixels = parseInt(e.target.value);
-                this.refresh();
-                this.store();
-            },
-            toggleHighlightColorShade() {
-                this.highlightColorShade = !this.highlightColorShade;
-                this.refresh();
-                this.store();
-            },
-            toggleReduceUnmatchedOpacity() {
-                this.reduceUnmatchedOpacity = !this.reduceUnmatchedOpacity;
-                this.refresh();
-                this.store();
-            },
-            toggleOutlineMatched() {
-                this.outlineMatched = !this.outlineMatched;
-                this.refresh();
-                this.store();
-            },
-            toggleDesaturateUnmatched() {
-                this.desaturateUnmatched = !this.desaturateUnmatched;
-                this.refresh();
-                this.store();
-            },
-            updateOutlineColor(e) {
-                this.outlineColor = e.target.value;
-                this.refresh();
-                this.store();
-            },
-            togglePauseOnClick() {
-                this.pauseOnClick = !this.pauseOnClick;
-                this.store();
-            },
-            toggleColorTheme(e) {
-                this.colorTheme = e.target.value;
-                this.store();
-            },
-            toggleAnalyzerBackground(e) {
-                this.analyzerBackground = e.target.value === 'custom' ? '#888888' : e.target.value;
-                this.refresh();
-                this.store();
-            },
-            toggleLogTimings() {
-                this.logTimings = !this.logTimings;
-                this.store();
-            },
-            toggleUseCompatMode() {
-                this.useCompatMode = !this.useCompatMode;
-                this.store();
-            },
-            toggleShowShadePrefix() {
-                this.showShadePrefix = !this.showShadePrefix;
-                this.store();
-            },
-        }
+    document.querySelector('#changeShortCuts').addEventListener('click', e => {
+        e.preventDefault();
+        chrome.tabs.create({ url: 'chrome://extensions/shortcuts' });
     })
-})
+
+    document.querySelector('#resetSettings').addEventListener('click', e => {
+        e.preventDefault();
+        console.log('FFFS')
+
+        Object.entries(DEFAULTSTATE).forEach(([key, value]) => {
+            value = JSON.parse(JSON.stringify(value));
+            STATE[key] = value;
+
+        });
+
+        apply();
+    })
+
+
+});
