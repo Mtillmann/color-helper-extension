5b0a3c3381515caa71e860676f00e6fac0a51d7b
Martin Tillmann <mtillmann@gmail.com>
2024-02-11 11:14:24 +0100

    Update popup/index.html and popup/popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,27 @@
+import { PALETTE } from "../assets/icons.js";
+import * as ICONS from "../assets/icons.js";
+
 document.addEventListener('DOMContentLoaded', () => {
     document.documentElement.setAttribute('data-bs-theme', window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+
+    
+
+    const items = [
+        {
+            icon : ICONS.PALETTE,
+            title : "Colors & Shades",
+            action : "Selection",
+            shortcut : "Ctrl + Shift + C"
+        }
+    ];
+
+    for(let item of items){
+        let a = document.createElement('a');
+        a.classList.add('icon-link');
+        const nodes = [];
+        if(item.icon){
+         
+        }
+        
+
 });
