c88e9e9c1b206923db64cd758e38b0bb551a61f5
Martin Tillmann <mtillmann@gmail.com>
2024-02-11 14:09:44 +0100

    Update icons and render popup buttons

--- options/options.js
+++ options/options.js
@@ -1,3 +1,5 @@
+import { ARROW_UP, ARROW_DOWN } from "../assets/icons.js";
+
 let DEFAULTSTATE = {
     //this is defined in background/index.js
 };
@@ -31,6 +33,8 @@ const STATE = {
     analyzerBackground: '#888888',
     showCustomAnalyzerBackgroundPicker: false,
 
+    popupGroups: [],
+
     logTimings: false,
 
     useCompatMode: false,
@@ -45,6 +49,70 @@ const STATE = {
 
 
     VERSION: chrome.runtime.getManifest().version
+}
+
+function renderPopupButtons() {
+    
+    const target = document.querySelector('[data-tab="popup"]');
+    target.innerHTML = '';
+
+    for (let i = 0; i < STATE.popupGroups.length; i++) {
+        const group = STATE.popupGroups[i];
+
+        target.insertAdjacentHTML('beforeend', `
+        <h3>${group.name}</h3>
+        <table class="table table-sm">
+        <thead>
+          <tr>
+            <th class="w-100">Action</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>`);
+
+      const tbody = target.querySelectorAll('tbody')[i];
+
+      const l = group.items.length;
+      for(let j = 0; j < l; j++){
+        const item = group.items[j];
+        tbody.insertAdjacentHTML('beforeend', `<tr data-group="${i}" data-index="${j}">
+          <td>
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" ${item.show ? 'checked' : ''} data-bind-popup-property="${i},${j}" id="cb-${i}-${j}">
+            <label class="form-check-label" for="cb-${i}-${j}">
+                ${item.action ?? group.name}
+            </label>
+          </div>
+          
+          </td>
+          
+          <td class="up-cell"></td>
+          <td class="down-cell"></td>
+        </tr>`);
+
+        if(l === 1){
+            continue;
+        }
+
+        const upCell = tbody.querySelectorAll('.up-cell')[j];
+        const downCell = tbody.querySelectorAll('.down-cell')[j];
+
+
+        if( j > 0){
+            upCell.insertAdjacentHTML('beforeend', `<a href="#" class="move-button up">${ARROW_UP}</a>`);
+        }
+
+        if(j < l - 1){
+            downCell.insertAdjacentHTML('beforeend', `<a href="#" class="move-button down">${ARROW_DOWN}</a>`);
+        }
+
+      }
+
+    }
 }
 
 function apply() {
@@ -108,11 +176,11 @@ async function store() {
 
 function changeTab(tab) {
     if (currentTab) {
-        document.querySelector(`[data-target="${currentTab}"]`).classList.remove('active','bg-primary-subtle');
+        document.querySelector(`[data-target="${currentTab}"]`).classList.remove('active', 'bg-primary-subtle');
         document.querySelector(`[data-tab="${currentTab}"]`).classList.add('d-none');
     }
     currentTab = tab;
-    document.querySelector(`[data-target="${currentTab}"]`).classList.add('active','bg-primary-subtle');
+    document.querySelector(`[data-target="${currentTab}"]`).classList.add('active', 'bg-primary-subtle');
     document.querySelector(`[data-tab="${currentTab}"]`).classList.remove('d-none');
 
 
@@ -128,11 +196,42 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     DEFAULTSTATE = settings.defaultState;
 
+    renderPopupButtons();
+
+    document.addEventListener('click', e => {
+        
+        if(e.target.closest('.move-button')){
+            const t = e.target.closest('.move-button');
+            e.preventDefault();
+            const tr = t.closest('tr');
+            const group = tr.dataset.group;
+            const index = tr.dataset.index;
+            const direction = t.classList.contains('up') ? -1 : 1;
+            const item = STATE.popupGroups[group].items.splice(index, 1)[0];
+            STATE.popupGroups[group].items.splice(index + direction, 0, item);
+            renderPopupButtons();
+            apply();
+        }
+    });
 
     ['change', 'input'].forEach(eventName => {
         document.addEventListener(eventName, e => {
+            if('bindPopupProperty' in e.target.dataset){
+                const group = parseInt(e.target.closest('tr').dataset.group);
+                const index = parseInt(e.target.closest('tr').dataset.index);
+                STATE.popupGroups[group].items[index].show = e.target.checked;
+                apply();
+                return;
+            }
+
+
             const targetType = e.target.getAttribute('type');
             const property = targetType === 'radio' ? e.target.name : e.target.dataset.bindProperty;
+
+            if (!property) {
+                return;
+            }
+
             const hint = e.target.dataset.typeHint;
 
             let value = targetType === 'checkbox' ? e.target.checked : e.target.value;
@@ -169,7 +268,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         apply();
     })
 
-    if(!currentTab){
+    if (!currentTab) {
         changeTab(document.querySelector('#verticalTabs a[data-target="popup"]').dataset.target);
     }
 
